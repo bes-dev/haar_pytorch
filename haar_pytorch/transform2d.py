6ea51aca235f2d52c82567524a69c607866945ef
BeS <sergei.o.belousov@gmail.com>
2021-11-16 23:42:53 +0300

    bugfix

--- haar_pytorch/transform2d.py
+++ haar_pytorch/transform2d.py
@@ -61,8 +61,8 @@ class HaarInverse(nn.Module):
         size = [x.shape[0], x.shape[1] // 4, x.shape[2] * 2, x.shape[3] * 2]
         f = lambda i: x[:, size[1] * i : size[1] * (i + 1)]
         out = torch.zeros(size, dtype=x.dtype, device=x.device)
-        out[:,:,0::2,0::2] = alpha * (f(0) + f(1) + f(2) + f(3))
-        out[:,:,0::2,1::2] = alpha * (f(0) + f(1) - f(2) - f(3))
-        out[:,:,1::2,0::2] = alpha * (f(0) - f(1) + f(2) - f(3))
-        out[:,:,1::2,1::2] = alpha * (f(0) - f(1) - f(2) + f(3))
+        out[:,:,0::2,0::2] = self.alpha * (f(0) + f(1) + f(2) + f(3))
+        out[:,:,0::2,1::2] = self.alpha * (f(0) + f(1) - f(2) - f(3))
+        out[:,:,1::2,0::2] = self.alpha * (f(0) - f(1) + f(2) - f(3))
+        out[:,:,1::2,1::2] = self.alpha * (f(0) - f(1) - f(2) + f(3))
         return out
